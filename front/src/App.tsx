bc20b7b27eda64541a4d162acd53916b879a5bc6
raahma <rahmajlassi2004@gmail.com>
2024-03-21 11:46:26 +0100

    Create Header

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,12 +1,17 @@
 import { Redirect, Route } from "react-router-dom";
 import {
   IonApp,
+  IonContent,
+  IonHeader,
   IonIcon,
   IonLabel,
+  IonMenu,
   IonRouterOutlet,
   IonTabBar,
   IonTabButton,
   IonTabs,
+  IonTitle,
+  IonToolbar,
   setupIonicReact,
 } from "@ionic/react";
 import { IonReactRouter } from "@ionic/react-router";
@@ -14,8 +19,6 @@ import { ellipse, home, square, triangle } from "ionicons/icons";
 import Tab1 from "./pages/Home";
 import Tab2 from "./pages/Categories";
 import Tab3 from "./pages/Contact";
-
-
 
 /* Core CSS required for Ionic components to work properly */
 import "@ionic/react/css/core.css";
@@ -44,6 +47,16 @@ setupIonicReact();
 const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
+      <IonMenu contentId="main-content" slot="start">
+        <IonHeader>
+          <IonToolbar>
+            <IonTitle>Menu Content</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent className="ion-padding">
+          This is the menu content.
+        </IonContent>
+      </IonMenu>
       <IonTabs>
         <IonRouterOutlet>
           <Route exact path="/home">
